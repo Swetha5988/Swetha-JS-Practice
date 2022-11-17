00582719e507397a633be6dd5a1a263d5e9a0b6f
Swetha D <dswethatwinkle@gmail.com>
2022-11-17 21:34:32 +0530

    calculator task

--- small tasks practice/calculator/script.js
+++ small tasks practice/calculator/script.js
@@ -1,10 +1,56 @@
-function customNumButton(number) {
-  const list = document.getElementById("btn").classList;
-  list.add("numButtonStyle");
-  return `<button type="button" id="numButtonStyle">${number}</button>`;
+document.addEventListener("DOMContentLoaded", function () {});
+
+function customButton(buttonText, className) {
+  return `<button type="button" class="${className}">${buttonText}</button>`;
 }
-function allNum(digit) {
-  for (let i = 0; i < digit.length; i++) {
-    return customNumButton(digit);
+
+function allNum() {
+  const arrc = Array.from(Array(10).keys()).concat([".", "="]);
+  const buttons = document.getElementById("buttons");
+  for (let i = 0; i < arrc.length; i++) {
+    buttons.insertAdjacentHTML(
+      "beforeend",
+      customButton(arrc[i], "numButtonStyle")
+    );
   }
 }
+function main() {
+  allNum();
+  symbols();
+  const elements = document.querySelectorAll(".numButtonStyle");
+  let btnsArr = Array.from(elements);
+  btnsArr.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      console.log(event.target.innerHTML);
+    });
+  });
+}
+function symbols() {
+  const arr = ["+", "-", "*", "/"];
+  const symbols = document.getElementById("symBtn");
+  for (let i = 0; i < arr.length; i++) {
+    symbols.insertAdjacentHTML(
+      "beforeend",
+      customButton(arr[i], "symButtonStyle")
+    );
+  }
+}
+// click event on multiple elements
+
+// operations on elements
+
+// function operations(a, b, operator) {
+//   operator(a, b);
+// }
+// function add(a, b) {
+//   return a + b;
+// }
+// function sub(a, b) {
+//   return a - b;
+// }
+// function division(a, b) {
+//   return a / b;
+// }
+// function multip(a, b) {
+//   return a * b;
+// }
