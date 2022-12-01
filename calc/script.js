489e49fc7b54aaa08db69865ed2b429dfbd01b62
Swetha D <dswethatwinkle@gmail.com>
2022-12-01 11:21:56 +0530

    calc

--- calc/script.js
+++ calc/script.js
@@ -3,128 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {});
 function customButton(buttonText, className) {
   return `<button type="button" class="${className}">${buttonText}</button>`;
 }
-// function allNum() {
-//   let arrc = Array.from(Array(10).keys()).concat([".", "="]);
-//   const buttons = document.getElementById("buttons");
-//   for (let i = 0; i < arrc.length; i++) {
-//     buttons.insertAdjacentHTML(
-//       "beforeend",
-//       customButton(
-//         arrc[i],
-//         arrc[i] === "=" ? "symButtonStyle" : "numButtonStyle"
-//       )
-//     );
-//   }
-// }
-
-// function symbols() {
-//   const arr = ["+", "-", "*", "/"];
-//   const symbols = document.getElementById("symBtn");
-//   for (let i = 0; i < arr.length; i++) {
-//     symbols.insertAdjacentHTML(
-//       "beforeend",
-//       customButton(arr[i], "symButtonStyle")
-//     );
-//   }
-// }
-// function main() {
-//   allNum();
-//   symbols();
-//   const elements = document.querySelectorAll(".numButtonStyle");
-//   const symb = document.querySelectorAll(".symButtonStyle");
-//   let btnsArr = Array.from(elements);
-//   let sArr = Array.from(symb);
-
-//   btnsArr.forEach((element) => {
-//     element.addEventListener("click", (event) => {
-//       document.getElementById("display-text").innerHTML +=
-//         event.target.innerHTML;
-//     });
-//   });
-
-//   sArr.forEach((symbs) => {
-//     symbs.addEventListener("click", (e) => {
-//       // let operator = e.target.innerHTML;
-//       if (e.target.innerHTML !== "=") {
-//         return (document.getElementById("display-text").innerHTML +=
-//           e.target.innerHTML);
-//       } else {
-//         let expression = document.getElementById("display-text").innerHTML;
-//         document.getElementById("display-text").innerHTML = eval(expression);
-//       }
-//     });
-//   });
-// }
-
-let clearButton = document.getElementById("clear");
-clearButton.addEventListener("click", clearAll);
-function clearAll() {
-  document.getElementById("display-text").innerHTML = "";
-}
-
-//Event handler for multiply/divide buttons
-
 class CalculatorStore {
-  allNum = () => {
-    let arrc = Array.from(Array(10).keys()).concat([".", "="]);
-    const buttons = document.getElementById("buttons");
-    for (let i = 0; i < arrc.length; i++) {
-      buttons.insertAdjacentHTML(
-        "beforeend",
-        customButton(
-          arrc[i],
-          arrc[i] === "=" ? "symButtonStyle" : "numButtonStyle"
-        )
-      );
-    }
-  };
-  symbols = () => {
-    const arr = ["+", "-", "*", "/"];
-    const symbols = document.getElementById("symBtn");
-    for (let i = 0; i < arr.length; i++) {
-      symbols.insertAdjacentHTML(
-        "beforeend",
-        customButton(arr[i], "symButtonStyle")
-      );
-    }
-  };
-  main = () => {
-    this.allNum();
-    this.symbols();
-    const elements = document.querySelectorAll(".numButtonStyle");
-    const symb = document.querySelectorAll(".symButtonStyle");
-    let btnsArr = Array.from(elements);
-    let sArr = Array.from(symb);
-
-    btnsArr.forEach((element) => {
-      element.addEventListener("click", (event) => {
-        document.getElementById("display-text").innerHTML +=
-          event.target.innerHTML;
-      });
-    });
-
-    sArr.forEach((symbs) => {
-      symbs.addEventListener("click", (e) => {
-        let operator = e.target.innerHTML;
-        if (e.target.innerHTML !== "=") {
-          return (document.getElementById("display-text").innerHTML +=
-            e.target.innerHTML);
-        } else {
-          // let expression = document.getElementById("display-text").innerHTML;
-          document.getElementById("display-text").innerHTML =
-            this.evaluateFunction(op1, op2, operator);
-        }
-      });
-    });
-  };
   addSub = [];
   mulDiv = [];
   //Reference to the array that was last updated.
   lastUpdated = null;
 
   evaluateFunction(op1, op2, operator) {
-    let intOp1 = parseInt(op1);
-    let intOp2 = parseInt(op2);
+    let intOp1 = parseFloat(op1);
+    let intOp2 = parseFloat(op2);
     switch (operator) {
       case "+":
         return intOp1 + intOp2;
@@ -139,38 +26,40 @@ class CalculatorStore {
 
   addSubFunction(operatorInput, operandInput) {
     if (this.lastUpdated == null) {
+      // last updated is if any values has beed generated in an array
       this.addSub.push(operandInput);
       this.addSub.push(operatorInput);
-    } else {
+    } else if (this.lastUpdated == this.mulDiv) {
+      // last updated is muldiv; evaluate expression using muldiv and add the result to addSub
+
       let result = this.evaluateFunction(
         this.lastUpdated[0],
         operandInput,
         this.lastUpdated[1]
-      ); //Evaluate only lastUpdated
+      );
 
-      if (this.lastUpdated == this.mulDiv) {
-        //if mulDiv array
-        this.lastUpdated.shift();
-        this.lastUpdated.shift();
-        if (this.addSub.length > 0) {
-          this.addSub[0] = this.evaluateFunction(
-            this.addSub[0],
-            result,
-            this.addSub[1]
-          );
-          this.addSub[1] = operatorInput;
-        } else {
-          this.addSub[0] = result;
-          this.addSub[1] = operatorInput;
-        }
+      if (this.addSub.length > 0) {
+        this.addSub[0] = this.evaluateFunction(
+          this.addSub[0],
+          result,
+          this.addSub[1]
+        );
+      } else {
+        this.addSub[0] = result;
       }
-      // else {
-      //   this.addSub[0] = result;
-      //   this.addSub[1] = operatorInput;
-      // }
+      this.mulDiv.length = 0;
+      this.addSub[1] = operatorInput;
+    } else {
+      // last updated is add sub; evaluate the expression using addSub and add the result to addSub
+      this.addSub[0] = this.evaluateFunction(
+        this.lastUpdated[0],
+        operandInput,
+        this.lastUpdated[1]
+      );
+      this.addSub[1] = operatorInput;
     }
-
     this.lastUpdated = this.addSub;
+
     console.log(this.addSub);
   }
 
@@ -189,17 +78,114 @@ class CalculatorStore {
     this.lastUpdated = this.mulDiv;
     console.log(this.mulDiv);
   }
+  getResult() {
+    return this.addSub[0];
+  }
+  reset() {
+    this.addSub.length = 0;
+    this.mulDiv.length = 0;
+    this.lastUpdated = null;
+  }
+}
+function allNum() {
+  let arrc = Array.from(Array(10).keys()).concat([".", "="]);
+  const buttons = document.getElementById("buttons");
+  for (let i = 0; i < arrc.length; i++) {
+    buttons.insertAdjacentHTML(
+      "beforeend",
+      customButton(
+        arrc[i],
+        arrc[i] === "=" ? "symButtonStyle" : "numButtonStyle"
+      )
+    );
+  }
+}
+
+function symbols() {
+  const arr = ["+", "-", "*", "/"];
+  const symbols = document.getElementById("symBtn");
+  for (let i = 0; i < arr.length; i++) {
+    symbols.insertAdjacentHTML(
+      "beforeend",
+      customButton(arr[i], "symButtonStyle")
+    );
+  }
 }
 let op = new CalculatorStore();
-// 15/5+6-8*4=
-//--- 3+6-32= -23
-// 8*4/2+6-1
-// 21
-// 4 + (6 / 2) * 3;
-// 4+ 9=13
-// op.mulDivFunction("*", "8");
-// op.mulDivFunction("/", "4");
-op.addSubFunction("+", "4");
-op.mulDivFunction("/", "6");
-op.mulDivFunction("*", "2");
-op.addSubFunction("+", "3");
+function main() {
+  let isOperatorClicked = true;
+  let sign = false;
+  let operandClicked = false;
+  allNum();
+  symbols();
+  const elements = document.querySelectorAll(".numButtonStyle");
+  const symb = document.querySelectorAll(".symButtonStyle");
+  let btnsArr = Array.from(elements);
+  let sArr = Array.from(symb);
+
+  btnsArr.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      operandClicked = true;
+      if (isOperatorClicked) {
+        document.getElementById("display-text").innerHTML =
+          event.target.innerHTML;
+        isOperatorClicked = false;
+      } else {
+        document.getElementById("display-text").innerHTML +=
+          event.target.innerHTML;
+      }
+    });
+  });
+
+  sArr.forEach((symbs) => {
+    symbs.addEventListener("click", (e) => {
+      let operator = e.target.innerHTML;
+      if (operator == "-" && !operandClicked && isOperatorClicked) {
+        sign = true;
+        isOperatorClicked = false;
+      }
+
+      let valueOfDisplay = document.getElementById("display-text").innerHTML;
+
+      if (sign) {
+        if (operandClicked) {
+          document.getElementById("display-text").innerHTML =
+            "-" + valueOfDisplay;
+          sign = false;
+          isOperatorClicked = true;
+          operandClicked = false;
+        }
+      } else {
+        isOperatorClicked = true;
+      }
+
+      if (isOperatorClicked) {
+        valueOfDisplay = document.getElementById("display-text").innerHTML;
+        if (operator == "=" || operator == "+" || operator == "-") {
+          op.addSubFunction(operator, valueOfDisplay);
+          document.getElementById("display-text").innerHTML = op.getResult();
+        } else {
+          op.mulDivFunction(operator, valueOfDisplay);
+        }
+      }
+    });
+  });
+}
+
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearAll);
+function clearAll() {
+  document.getElementById("display-text").innerHTML = "";
+  op.reset();
+}
+
+//Event handler for multiply/divide buttons
+
+// 4*2+6/2*3=
+// 4*2+3*3=
+// 8+9 = 17
+// op.mulDivFunction("-", "4");
+// op.addSubFunction("+", "2");
+// op.mulDivFunction("/", "6");
+// op.mulDivFunction("*", "2");
+// op.addSubFunction("+", "3");
