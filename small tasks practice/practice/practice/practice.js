61a0398521f218f9e8ec25e5825b95af0aa84e57
Swetha D <dswethatwinkle@gmail.com>
2022-12-08 20:07:56 +0530

    second max

--- small tasks practice/practice/practice/practice.js
+++ small tasks practice/practice/practice/practice.js
@@ -98,3 +98,23 @@
 // }
 // console.log(sum);
 // console.log(add);
+
+// let arr = [17, 82, 3, 94, 85];
+// let max = null;
+// let sMax = null;
+// for (let i = 0; i < arr.length; i++) {
+//   if (max == null) {
+//     max = arr[0];
+//     // console.log(max);
+//   }
+//   if (arr[i] > max) {
+//     sMax = max;
+//     // console.log(sMax);
+//     max = arr[i];
+//   }
+//   if (arr[i] < max && arr[i] > sMax) {
+//     sMax = arr[i];
+//   }
+// }
+
+// console.log(sMax);
